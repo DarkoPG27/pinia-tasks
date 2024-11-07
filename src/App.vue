a575dd701e11096e685d6552ae0b84851a92029c
DarkoPg27 <darkobrnovic27@gmail.com>
2024-11-07 08:37:59 +0100

    added header and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template>App</template>
+<template>
+  <main>
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
